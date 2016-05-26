c97609bc35de4cf61adb5786aeddbf72a8163804
HKY <starmonhong@gmail.com>
2016-05-26 09:03:06 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Termproject::Application.config.secret_key_base = '994fa5fd1122295b82d090007911e432a87aaac0a190021da7f8e492a135b14427257bb7f9a2073345f92d2dab23364aeb646ca5fb43aad7f82493df0f53b1bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Termproject::Application.config.session_store :cookie_store, key: '_termproject_session'
